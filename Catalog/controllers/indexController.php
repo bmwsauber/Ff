380f0f2b8a568d25da8adce2dda253b299c219f0
Alex Du <dubovis77@gmail.com>
2013-06-04 12:55:43 +0300

    dev

---
+++ Catalog/controllers/indexController.php
@@ -0,0 +1,41 @@
+<?php
+class Catalog_controllers_IndexController
+{
+    public function indexAction()
+    {
+        var_dump('asdasdasd');
+    }
+
+
+    public function initAction()
+    {
+
+
+        /**********************EXAMPLES****************/
+
+        //$location = Ff::getRequest()->getParam('location');
+        //$res = Ff::getModel('location/location')->load($location);
+        //$res = Ff::getModel('location/location')->load(array(1,2,3));
+
+        //~ $res = Ff::getModel('location/location')->getCollection()->addFieldToFilter('color', 'red', '!=');
+        //~ foreach($res as $re)
+        //~ {
+           //~ var_dump($re->id);
+        //~ }
+
+        /***********************************************/
+
+        //~ $model = Ff::getModel('location/location');
+        //~ 
+        //~ $data = array('color' => array('pink', 'blue', 'yellow'), 'status' => array('1','2','3'));
+        //~ 
+        //~ $model->setData($data);
+        //~ $model->setId(null);
+        //~ $model->save();
+        
+        /************************************************/
+
+
+    }
+
+}
